d31a7437c123f58963abdbe7b0acfa685ea5e8b7
ssasan <ssasan@asb9840u-b06.csil.sfu.ca>
2019-10-19 20:21:16 -0700

    read??..not sure what to do with this yet

--- encoder.cpp
+++ encoder.cpp
@@ -1,4 +1,4 @@
-#include <iostream?
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -18,8 +18,26 @@ int main(int argc, char* argv[])
 	}
 
     string inFile = argv[1];         // string variable to get the input file format
-    string outFile = argv[2];        // string variable to get the output file format
+//    string outFile = argv[2];        // string variable to get the output file format
 
     ifstream inStream;  //input file stream
     ofstream outStream; //output file stream
+
+    inStream.open(inFile, ios::binary);
+
+    double value;
+    int i = 0;
+    char buf[sizeof(double)];
+    while (inStream.read(buf, sizeof(buf)))
+    {
+        memcpy(&value, buf, sizeof(value));
+        std::cout << value << " ";
+        i++;
+    }
+
+    std::cout << endl << "Total count: " << i << endl;
+
+    inStream.close();
+
+    return 0;
 }
