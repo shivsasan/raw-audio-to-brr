6b27746fc65ececc1507e572b7458b07f2256de6
ssasan <ssasan@asb9700u-d03.csil.sfu.ca>
2019-10-22 22:31:55 -0700

    do

--- encoder.cpp
+++ encoder.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+char nibba[9];
+
 string convertToString(char* a, int size) 
 { 
     int i; 
@@ -16,23 +18,32 @@ string convertToString(char* a, int size)
         s = s + a[i]; 
     } 
     return s; 
-} 
+}
 
 void quantize(int arr[], int range)
 {
-    cout<<"quaztize1"<<endl;
     int stepsize[13] = {1,2,4,8,16,32,64,256,512,1024,2048,4096};
+//    char arrHex[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
     for (int i = 0; i < 16 ; i++)
     {
-        cout<<"arr["<<i<<"]: "<<arr[i]<<endl;
-        cout<<"stepsize: "<<stepsize[range]<<endl;
-        cout<<"range : "<<range<<endl;
         arr[i] = arr[i]/(stepsize[range]);
-        cout<<arr[i]<<" ";
+        /*for(int i = 0; i<=15 ; i++)
+        {
+            
+        }*/
     }
-    cout<<"quaztize2"<<endl;
-
 }
+
+/*void makeNibble(int srcArr[], int decArr[])
+{
+//    char arrHex[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+//    int arrNib[] = {0000,0001,0010,0011,0100,0101,0110,0111,1000,1001,1010,1011,1100,1101,1110,1111};
+//    short int haha;
+    for (int i = 0; i <= 16; i++)
+    {
+        
+    }
+}*/
 
 int main(int argc, char* argv[])
 {
@@ -62,6 +73,12 @@ int main(int argc, char* argv[])
     int iter = 0;
     int max, min;
     int range;
+    string nibble;
+//    char hit;
+    bitset <4> nib;
+    int fiveCount = 0;
+    int fortyCount = 0;
+    
 //    int step_size[13] = {1,2,4,8,16,32,64,256,512,1024,2048,4096};
 
     while(inStream.read(reinterpret_cast < char*> (temp), sizeof(float)))
@@ -91,27 +108,81 @@ int main(int argc, char* argv[])
             max = *max_element(currentArr,currentArr+16);
             min = *min_element(currentArr,currentArr+16);
 
-            cout<<"max: " << max<<" min: "<<min<<endl; 
+//            cout<<"max: " << max<<" min: "<<min<<endl; 
 
             //range here
 
-            if ( max <= 7 && min >= -8 ){range = 0;}
-            if ( max <= 14 && min >= -16 ){range = 1;}
-            if ( max <= 28 && min >= -32 ){range = 2;}
-            if ( max <= 56 && min >= -64 ){range = 3;}
-            if ( max <= 112 && min >= -128 ){range = 4;}
-            if ( max <= 224 && min >= -256 ){range = 5;}
-            if ( max <= 448 && min >= -512 ){range = 6;}
-            if ( max <= 896 && min >= -1024 ){range = 7;}
-            if ( max <= 1792 && min >= -2048 ){range = 8;}
-            if ( max <= 3584 && min >= -4096 ){range = 9;}
-            if ( max <= 7168 && min >= -8192 ){range = 10;}
-            if ( max <= 14336 && min >= -16384 ){range = 11;}
-            if ( max <= 28672 && min >= -32768 ){range = 12;}
-            else{cout<<"OUT OF RANGE!!!"<<endl;}
+            if ( max >= -32768 && max <= 28672 && min <= 28672 && min >= -32768 )
+            {
+                range = 12;
+                if ( max <= 14336 && max >= -16384 && min <= 14336 && min >= -16384 )
+                {
+                    range = 11;
+                    if ( max <= 7168 && max >= -8192 && min <= 7168 && min >= -8192 )
+                    {
+                        range = 10;
+                        if ( max <= 3584 && max >= -4096 && min <= 3584 && min >= -4096 )
+                        {
+                            range = 9;
+                            if ( max <= 1792 && max >= -2048 && min <= 1792 && min >= -2048 )
+                            {
+                                range = 8;
+                                if ( max <= 896 && max >= -1024 && min <= 896 && min >= -1024 )
+                                {
+                                    range = 7;
+                                    if ( max <= 448 && max >= -512 && min <= 448 && min >= -512 )
+                                    {
+                                        range = 6;
+                                        if ( max <= 224 && max >= -256 && min <= 224 && min >= -256 )
+                                            {
+                                                range = 5;
+                                                if ( max <= 112 && max >= -128 && min <= 112 && min >= -128 ){
+                                                    range = 4;
+                                                    if ( max <= 56 && max >= -64 && min <= 56 && min >= -64 ){
+                                                        range = 3;
+                                                        if ( max <= 28 && max >= -32 && min <= 28 && min >= -32 ){
+                                                            range = 2;
+                                                            if ( max <= 14 && max >= -16 && min <= 14 && min >= -16 ){
+                                                                range = 1;
+                                                                if ( max <= 7 && max >= -8 && min <= 7 && min >= -8 ){
+                                                                    range = 0;}}}}}}}}}}}}}
+            else if (max > 28692 || max < -38768 || min < -32768 || min > 28692) {cout<<"OUT OF RANGE!!!"<<endl;return 0;}
+            //cout<<"range: "<<range<<endl;
 
             //if (range != 0){quantize(currentArr,range);} //quantizing to nibble
+
             quantize(currentArr,range);
+            for(int i = 0 ; i <= 15 ; i++)
+            {
+                if (currentArr[i] == 0){outStream << "0";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 1){outStream << "1";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 2){outStream << "2";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 3){outStream << "3";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 4){outStream << "4";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 5){outStream << "5";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 6){outStream << "6";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 7){outStream << "7";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 8){outStream << "8";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 9){outStream << "9";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 10){outStream << "a";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 11){outStream << "b";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 12){outStream << "c";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 13){outStream << "d";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 14){outStream << "e";fiveCount++;fortyCount++;}
+                if (currentArr[i] == 15){outStream << "f";fiveCount++;fortyCount++;}
+
+                //outStream << hit;
+                if(fiveCount==4)
+                {
+                    outStream<< " ";
+                    fiveCount = 0;
+                    if (fortyCount == 40)
+                    {
+                        outStream<< "\n";
+                        fortyCount = 0;
+                    }
+                }
+            }
 
             continue;
         }
@@ -133,6 +204,7 @@ int main(int argc, char* argv[])
     }
 
     inStream.close();       //close connection with file
+    outStream.close();
 
     return 0;
 }
---
+++ psnr.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+	ifstream oldInFile("sample_sound.bin", fstream::in | ifstream::binary);
+	ifstream convertedFile("sample_sound.bin", fstream::in | ifstream::binary);
+
+	unsigned char temp[sizeof(float)];
+	unsigned char temp2[sizeof(float)];
+    float floVar;
+    float otherFloVar;
+    int j = 0;
+    vector<float> newValue;
+    vector<float> oldValue;
+    float mse = 0;
+    float psnr = 0;
+    float max = powf(2,31);
+    float first_half = 0;
+	float second_half = 0;
+
+	while(convertedFile.read(reinterpret_cast<char*>(temp), sizeof(float)))
+	{
+
+		unsigned char t = temp[0];
+    	temp[0] = temp[3];
+	    temp[3] = t;
+	    t = temp[1];
+	    temp[1] = temp[2];
+	    temp[2] = t;
+	    floVar = reinterpret_cast<float&>(temp);
+		
+	    if (floVar > 1 || floVar < -1)
+	    {
+	    	cout << "OUT OF BOUNDS!!" << endl;
+	    	break;
+	    }
+
+	    newValue.push_back(floVar);
+
+	    oldInFile.read(reinterpret_cast<char*>(temp2), sizeof(float));
+
+	    unsigned char t2 = temp2[0];
+    	temp2[0] = temp2[3];
+	    temp2[3] = t2;
+	    t2 = temp2[1];
+	    temp2[1] = temp2[2];
+	    temp2[2] = t2;
+	    otherfloVar = reinterpret_cast<float&>(temp2);
+	    if(otherFloVar > 1 || otherFloVar < -1)
+	    {
+	    	cout << "wrong old 32 bits float input" << endl;
+	    	break;
+	    }
+	    oldValue.push_back(otherFloVar);
+	    j++;
+
+	}
+	for (int i=0;i<j;i++)
+	{
+		mse = static_cast<float>(mse) + powf((static_cast<float>(oldValue[i]) - static_cast<float>(newValue[i])),2);
+	}
+	mse = static_cast<float>(mse)/static_cast<float>(j);
+	first_half = static_cast<float>(20) * log10(max);
+	second_half = static_cast<float>(10) * log10(static_cast<float>(mse));
+	psnr = static_cast<float>(first_half) - static_cast<float>(second_half);
+	cout << psnr << endl;
+
+	oldInFile.close();
+	convertedFile.close();
+	return 0;
+}
